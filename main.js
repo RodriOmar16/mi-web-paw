31f5cc9c33071cc254278f69bde51bf99b9f3303
RodriOmar16 <rodrigoomarmiranda1@gmail.com>
2025-08-27 16:28:23 -0300

    2708025_16-28

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/*
+  Clase 1
+*/
+/*
 let a = 10;
 let b = 2;
 let c;
@@ -29,3 +33,48 @@ function encontrarMayor(a,b){
   return a > b ? a : b
 }
 console.log("el mayor de 10 y 11 es : "+encontrarMayor(100,11))
+*/
+
+/*
+  clase 2
+*/
+
+let arrayNrosEnteros = [1,2,3,4,5,6,7,8,9,10,11];
+let acu = 0;
+/*arrayNrosEnteros.forEach(e => {
+  acu += e;
+});*/
+arrayNrosEnteros.forEach(function(element){
+  acu += element;
+});
+console.log("la suma es: ", acu);
+
+/*let obj = {
+  pares: 0,
+  impares: 0
+}
+arrayNrosEnteros.forEach(e => {
+  if(e % 2 == 0){
+    obj.pares += 1;
+  }else obj.impares += 1;
+});
+console.log("cantidad de pares: ",obj.pares,"\tCantidad de impares: ",obj.impares);
+*/
+let cantPares = 0, cantImpares = 0;
+arrayNrosEnteros.forEach(function(element){
+  if(element % 2 == 0){
+    cantPares += 1;
+  }else cantImpares += 1;
+});
+console.log("cantidad de pares: ",cantPares,"\tCantidad de impares: ",cantImpares);
+
+let arrayString = ["hola", "mundo"];
+arrayString.forEach(function(elem, pos, arrayString){
+  arrayString[pos] = elem.toUpperCase();
+});
+/*let arrayString2 = [];
+
+arrayString.forEach(e => {
+  arrayString2.push(e.toUpperCase());
+});*/
+console.log("ahora nvo arrayString: ", arrayString);
